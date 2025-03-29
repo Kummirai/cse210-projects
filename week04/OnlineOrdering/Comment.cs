f65e61f1fffee50161d5188bcccb9178273c7d5c
Kummirai <ajaxmilton@hotmail.com>
2025-03-29 22:01:57 +0200

    Add Comment class for YouTubeTracker to handle commenter details

---
+++ week04/OnlineOrdering/Comment.cs
@@ -0,0 +1,17 @@
+// Comment.cs
+using System;
+
+namespace YouTubeTracker
+{
+    public class Comment
+    {
+        public string CommenterName { get; set; }
+        public string CommentText { get; set; }
+
+        public Comment(string name, string text)
+        {
+            CommenterName = name;
+            CommentText = text;
+        }
+    }
+}
