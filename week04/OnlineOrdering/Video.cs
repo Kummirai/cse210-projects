5696c55c3676c4f8fde213055b7eaeee744f4da8
Kummirai <ajaxmilton@hotmail.com>
2025-03-29 22:04:45 +0200

    Refactor Video class properties to use underscore prefix for private fields

--- week04/OnlineOrdering/Video.cs
+++ week04/OnlineOrdering/Video.cs
@@ -6,32 +6,32 @@ namespace YouTubeTracker
 {
     public class Video
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int LengthInSeconds { get; set; }
-        private List<Comment> Comments { get; set; }
+        public string _title { get; set; }
+        public string _author { get; set; }
+        public int _lengthInSeconds { get; set; }
+        private List<Comment> _comments { get; set; }
 
         public Video(string title, string author, int length)
         {
-            Title = title;
-            Author = author;
-            LengthInSeconds = length;
-            Comments = new List<Comment>();
+            _title = title;
+            _author = author;
+            _lengthInSeconds = length;
+            _comments = new List<Comment>();
         }
 
         public void AddComment(Comment comment)
         {
-            Comments.Add(comment);
+            _comments.Add(comment);
         }
 
         public int GetNumberOfComments()
         {
-            return Comments.Count;
+            return _comments.Count;
         }
 
         public List<Comment> GetAllComments()
         {
-            return new List<Comment>(Comments);
+            return new List<Comment>(_comments);
         }
     }
 }
