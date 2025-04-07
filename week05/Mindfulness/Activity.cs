0222aa3c51de7ecc479f3fb1dc057e24fd66d500
Kummirai <ajaxmilton@hotmail.com>
2025-04-07 23:42:11 +0200

    Add Activity class :mindfulness  base class for activities

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading;
+
+namespace MindfulnessApp
+{
+    public abstract class Activity
+    {
+        protected string Name { get; set; }
+        protected string Description { get; set; }
+        protected int Duration { get; set; }
+
+        public void Start()
+        {
+            Console.Clear();
+            Console.WriteLine($"Welcome to the {Name} Activity.");
+            Console.WriteLine();
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            Console.Write("How long, in seconds, would you like for your session? ");
+            Duration = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.WriteLine("Get ready to begin...");
+            ShowSpinner(3);
+        }
+
+        public void End()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Good job! You have completed another activity.");
+            ShowSpinner(3);
+            Console.WriteLine($"You have completed the {Name} activity for {Duration} seconds.");
+            ShowSpinner(3);
+        }
+
+        protected void ShowSpinner(int seconds)
+        {
+            for (int i = 0; i < seconds; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+        }
+
+        protected void ShowCountdown(int seconds)
+        {
+            for (int i = seconds; i > 0; i--)
+            {
+                Console.Write($"{i} ");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+        }
+
+        public abstract void Run();
+    }
+}
