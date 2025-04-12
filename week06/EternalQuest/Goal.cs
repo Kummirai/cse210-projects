731a40bb53fda4ce4b79557d6698b3ef6a56dcfb
Kummirai <ajaxmilton@hotmail.com>
2025-04-13 01:27:21 +0200

    Add abstract Goal class to define goal structure and behavior

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,27 @@
+namespace EternalQuest.Models
+{
+    public abstract class Goal
+    {
+        protected string _name;
+        protected string _description;
+        protected int _points;
+        protected bool _isComplete;
+
+        public string Name => _name;
+        public bool IsComplete => _isComplete;
+
+        public Goal(string name, string description, int points)
+        {
+            _name = name;
+            _description = description;
+            _points = points;
+            _isComplete = false;
+        }
+
+        public abstract void RecordEvent();
+        public abstract string GetProgress();
+        public abstract void DisplayGoal();
+
+        public virtual int GetPoints() => _points;
+    }
+}
